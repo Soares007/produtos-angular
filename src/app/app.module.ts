09e2466bb1c0395b977cf9342b3c8e8ee8be73f5
Soares007 <soaresstar947@gmail.com>
2023-05-02 18:04:13 -0300

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProdutosComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
